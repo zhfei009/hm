68a99d259265f943579d042d40ea64bbf0ab68f0
zfei <zhfei_cz@163.com>
2019-03-13 10:05:09 +0800

    done

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,17 @@ const webserver = require('gulp-webserver');
 const { readFileSync } = require('fs');
 const url = require('url');
 const path = require('path');
+const minHTML = require('gulp-htmlmin');
 const listData = require('./src/data/data.json');
 // 编译Sass
 gulp.task('mySass', () => {
     return gulp.src('./src/scss/**/*.scss')
         .pipe(sass())
-        .pipe(minCss())
         .pipe(gulp.dest('./src/css'));
 });
 // 监听sass
 gulp.task('watching', () => {
     gulp.watch('./src/scss/**/*.scss', gulp.series('mySass'));
-});
-// 压缩JS
-gulp.task('uglifyJS', () => {
-    return gulp.src('./src/js/**/*.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest('./build/js'));
 });
 
 // 启服务
@@ -49,3 +40,31 @@ gulp.task('server', () => {
             }
         }))
 });
+// 管理开发任务
+gulp.task('dev', gulp.series('mySass', 'server', 'watching'));
+
+
+// 上线时去压缩并打包
+// 压缩JS
+gulp.task('uglifyJS', () => {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('html', function() {
+    return gulp.src('./src/**/*.html')
+        .pipe(minHTML({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./build'));
+});
+gulp.task('css', function() {
+    return gulp.src('./src/css/**/*.css')
+        .pipe(minCss())
+        .pipe(gulp.dest('./build/css'));
+});
+
+// 管理上线任务
+gulp.task('build', gulp.parallel('uglifyJS', 'html', 'css'));
